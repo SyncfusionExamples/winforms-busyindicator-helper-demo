6d7a96585bc8ef9f407510f15be6b1083c85c6d7
Neelakandan K <neelakandan.kannan@syncfusion.com>
2018-08-10 12:53:48 +0530

    Sample has been committed

---
+++ CS/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace BusyIndicatorHelperSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sfButton1 = new Syncfusion.WinForms.Controls.SfButton();
+            this.SuspendLayout();
+            // 
+            // sfButton1
+            // 
+            this.sfButton1.AccessibleName = "Button";
+            this.sfButton1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F);
+            this.sfButton1.Location = new System.Drawing.Point(72, 63);
+            this.sfButton1.Name = "sfButton1";
+            this.sfButton1.Size = new System.Drawing.Size(248, 76);
+            this.sfButton1.TabIndex = 0;
+            this.sfButton1.Text = "Get Items";
+            this.sfButton1.Click += new System.EventHandler(this.sfButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 211);
+            this.Controls.Add(this.sfButton1);
+            this.Name = "Form1";
+            this.Text = "Busy Indicator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Syncfusion.WinForms.Controls.SfButton sfButton1;
+    }
+}
+
---
+++ CS/Form1.cs
@@ -0,0 +1,42 @@
+﻿using Syncfusion.WinForms.Core.Utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BusyIndicatorHelperSample
+{
+    public partial class Form1 : Form
+    {
+        BusyIndicator busyIndicator = new BusyIndicator();
+        ObservableCollection<int> sampleData = new ObservableCollection<int>();
+        public Form1()
+        {
+            InitializeComponent();           
+        }
+        private void sfButton1_Click(object sender, EventArgs e)
+        {
+            this.sfButton1.Text = string.Empty;
+            busyIndicator.Show(this.sfButton1);
+
+            //You can uncomment the below line if you want to show at any location.
+            //this.sfButton1.Text = "Loading";
+            //busyIndicator.Show(this.sfButton1, new Point((this.sfButton1.Width / 2) + this.busyIndicator.Image.Width, (this.sfButton1.Height / 2) - this.busyIndicator.Image.Height / 2));
+
+            for (int i = 0; i <= 10000000; i++)
+            {
+                sampleData.Add(i);
+            }
+            busyIndicator.Hide();
+            this.sfButton1.Text = "Get items";
+            sampleData.Clear();
+        }
+    }   
+}
